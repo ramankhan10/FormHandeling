cd81e2558a84711b8853f454671d6f3c697361a3
Raman <khanahmadi.raman@gmail.com>
2023-05-03 15:12:51 +0330

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FormHandeling';
+  
+  @ViewChild('f') signupProperty: NgForm | null = null;
+
+  onSubmit() {
+    console.log(this.signupProperty);
+  }
 }
